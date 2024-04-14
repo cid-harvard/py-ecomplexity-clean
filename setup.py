6f4e35008469398a903b67a5909b001a305887c7
Shreyas Gadgin Matha <shreyas.gm61@gmail.com>
2024-04-14 06:05:38 -0400

    Set up release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open("README.md") as f:
+    with open("README.md", encoding="utf-8") as f:
         return f.read()
 
 setup(name='ecomplexity',
-      version='0.5.2',
+      version='0.5.3',
       description='Package to calculate economic complexity and associated variables',
       long_description=readme(),
       long_description_content_type='text/markdown',
